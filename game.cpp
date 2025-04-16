8dcf0ecbf4cb1a0d8ea699b06fa3c91e65385a19
hthuong91 <24022741@vnu.edu.vn>
2025-04-16 23:32:16 +0700

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,66 @@
+#include "Game.h"
+using namespace std;
+
+Game::Game()
+{
+    running = true;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cerr << "SDL could not initialize! SDL_Error" << SDL_GetError() << endl;
+        running = false;
+    }
+    window = SDL_CreateWindow("Battle City", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+        running = false;
+    }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
+        running = false;
+    }
+    if (!renderer) {
+        cerr << "Renderer could not be created! SDL_Error:" ;
+        running = false;
+    }
+    generateWalls();
+
+}
+void Game::generateWalls() {
+    for (int i = 3; i < MAP_HEIGHT - 3; i += 20) {
+        for (int j = 3; j < MAP_WIDTH - 3; j += 2) {
+            wall w(j * TILE_SIZE, i * TILE_SIZE);
+            walls.push_back(w);
+        }
+    }
+}
+void Game::render()
+{
+    SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255); // ranh gi?i
+    SDL_RenderClear(renderer); // xóa màu
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    for (int i=1; i< MAP_HEIGHT - 1; i++) {
+        for (int j=1; j<MAP_WIDTH-1; j++) {
+            SDL_Rect tile = {j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE };
+            SDL_RenderFillRect(renderer, &tile);
+        }
+    }
+    for (size_t i = 0; i < walls.size(); i++) {
+        walls[i].render(renderer);
+    }
+    SDL_RenderPresent(renderer);
+}
+
+void Game::run()
+{
+    while (running) {
+        render();
+        SDL_Delay(16);
+    }
+}
+
+Game::~Game()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}

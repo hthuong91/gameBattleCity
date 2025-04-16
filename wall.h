ace724d7db0b01c7a0ec2cf81dcec579cc1f5a42
hthuong91 <24022741@vnu.edu.vn>
2025-04-16 23:32:29 +0700

    Create wall.h

---
+++ wall.h
@@ -0,0 +1,16 @@
+#ifndef WALL_H
+#define WALL_H
+#include <SDL.h>
+
+class wall
+{
+public:
+    int x, y;
+    SDL_Rect rect;
+    bool active;
+
+    wall(int startX, int startY);
+    void render(SDL_Renderer* renderer);
+};
+
+#endif

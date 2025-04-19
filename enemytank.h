b9f67b8a76a842d9602d0467f9fa2ebb13bf8c46
hthuong91 <24022741@vnu.edu.vn>
2025-04-19 16:50:24 +0700

    Create enemytank.h

---
+++ enemytank.h
@@ -0,0 +1,22 @@
+//#ifndef ENEMYTANK_H
+//#define ENEMYTANK_H
+
+
+//class enemytank
+//{
+    //public:
+        //int x, y;
+        //int dirX, dirY;
+        //int moveDelay, shoot Delay;
+        //SDL_Rect rect;
+        //bool active;
+        //vector<bullet> bullets;
+        //enemytank(int startX, int startY);
+        //void move(const vector<wall>& walls);
+        //void shoot();
+        //void updateBullets() ;
+        //void render(SDL_Renderer* renderer);
+//};
+
+
+//#endif // ENEMYTANK_H

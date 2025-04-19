92423e677c58fedfbff9065e417dfd1ad92c01d9
hthuong91 <24022741@vnu.edu.vn>
2025-04-19 22:42:43 +0700

    Update enemytank.h

--- enemytank.h
+++ enemytank.h
@@ -1,22 +1,28 @@
-//#ifndef ENEMYTANK_H
-//#define ENEMYTANK_H
+#ifndef ENEMYTANK_H
+#define ENEMYTANK_H
+#include<SDL.h>
+#include<vector>
+#include "bullet.h"
+#include "wall.h"
+using namespace std;
+class Game;
+class playertank;
+class bullet;
 
+class enemytank
+{
+    public:
+        int x, y;
+        int dirX, dirY;
+        int moveDelay, shootDelay;
+        SDL_Rect rect;
+        bool active;
+        vector<bullet> bullets;
+        enemytank(int startX, int startY);
+        void move(const vector<wall>& walls);
+        void shoot();
+        void updateBullets() ;
+        void render(SDL_Renderer* renderer);
+};
 
-//class enemytank
-//{
-    //public:
-        //int x, y;
-        //int dirX, dirY;
-        //int moveDelay, shoot Delay;
-        //SDL_Rect rect;
-        //bool active;
-        //vector<bullet> bullets;
-        //enemytank(int startX, int startY);
-        //void move(const vector<wall>& walls);
-        //void shoot();
-        //void updateBullets() ;
-        //void render(SDL_Renderer* renderer);
-//};
-
-
-//#endif // ENEMYTANK_H
+#endif // ENEMYTANK_H

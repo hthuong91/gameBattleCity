597632db28bbd27238f3f00a6b4ed471e2865da4
hthuong91 <24022741@vnu.edu.vn>
2025-04-24 03:56:15 +0700

    Create app.cpp

---
+++ app.cpp
@@ -0,0 +1,115 @@
+#include "app.h"
+#include "appconfig.h"
+#include "engine.h"
+#include "game.h"
+#include "menu.h"
+
+#include <ctime>
+#include <iostream>
+#include <stdlib.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+App::App()
+{
+    m_window = nullptr;
+}
+
+App::~App()
+{
+    if (m_app_state != nullptr)
+        delete m_app_state;
+}
+void App::run()
+{
+    is_running = true; //khởi tạo SDL và tạo cửa sổ
+
+    if(SDL_Init(SDL_INIT_VIDEO) == 0)
+    {
+        m_window = SDL_CreateWindow("TANKS", SDl_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                    AppConfig::windows_rect.w, AppCOnfig::windows_rect.h, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+        if(m_window == nullptr) return;
+        if(!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) return;
+        if(TTF_Init() == -1) return ;
+
+        srand(time(NULL)); // Khởi tạo bộ sinh số ngẫu nhiên( Kẻ địch xuất hiện ở vị trí bất kì, Hướng di chuyển của xe tăng địch, Power-up rơi ra ngẫu nhiên)
+
+        Engine& engine = Engine::getEngine();
+        engine.innitModules();
+        engine.getRenderer()->loadTexture(m_window);
+        engine.getRenderer()->loadFont();
+
+        m_app_state = new Menu;
+
+        double FPS;
+        Unit32 time1, time2, dt, fps_time =0, fps_count = 0, delay =15;
+        time1 = SDL_GetTicks();
+        while(is_running)
+        {
+            time2 = SDL_GetTicks();
+            dt= time2 - time1;
+            time1 = time2;
+            if (m_app_state->finished())
+            {
+                Appstate* new_state = m_app_state->nextState();
+                delete m_app_state;
+                m_app_state = new_state;
+            }
+            if(m_app_state == nullptr) break;
+
+            eventProces();
+
+            m_app_state->update(dt);
+            m_app_state->draw();
+
+            SDL_Delay(delay);
+
+            //FPS(khung hình mỗi giây)
+            fps_time += dt;
+            fps_count++;
+            if(fps_time > 200)
+            {
+                FPS = (double)fps_count / fps_time *1000;
+                if (FPS > 60) delay--;
+                else if(delay > 0) delay--;
+                fps_time = 0;
+                fps_count = 0;
+            }
+        }
+
+        engine.destroyModules();
+    }
+    SDL_DestroyWindow(m_window);
+    m_window = nullptr;
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
+}
+
+void App:eventProces()
+{
+    SDL_Event event;
+    while(SDL_PollEvent(&event))
+    {
+        if(event.type == SDL_QUIT)
+        {
+            is_running = false;
+        }
+        else if(event.type == SDL_WINDOWEVENT)
+        {
+            if(event.window.event ==SDL_WINDOWEVENT_RESZIEED ||
+               event.window.event == SDL_WINDOWEVENT_MAXIMIZED ||
+               event.window.event == SDL_WINDOWEVENT_RESTORED ||
+               event.window.event == SDL_WINDOWEVENT_SHOWN)
+            {
+                AppConfig::windows_rect.w = event.window.data1;
+                AppConfig::windows_rect.h = event.window.data2;
+                Engine::getEngine().getRenderer()->setScale((float)AppConfig::windows_rect.w / (AppConfig::map_rect.w + AppConfig::status_rect.w),
+                                                            (float)AppConfig::windows_rect.h / AppConfig::map_rect.h);
+            }
+        }
+
+        m_app_state->eventProcess(&event);
+    }
+}

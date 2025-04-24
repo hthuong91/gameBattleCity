b7b275260afb253ef736ec08d62804922826ae9b
hthuong91 <24022741@vnu.edu.vn>
2025-04-25 00:41:28 +0700

    Create object.h

---
+++ object.h
@@ -0,0 +1,32 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include "engine.h"
+
+class Object
+{
+public:
+    Object();
+    Object(double x, double y, SpriteType type);
+    Object(double x, double y, const SpriteData* sprite);
+    virtual ~Object();
+    virtual void draw();
+    virtual void update(Uint32 dt);
+    bool to_erase;
+    SDL_Rect collision_rect;
+    SDL_Rect dest_rect;
+    SDL_Rect src_rect;
+    SpriteType type;
+    double pos_x;
+    double pos_y;
+
+protected:
+    SDL_Rect moveRect(const SDL_Rect &rect, int x, int y);
+    const SpriteData* m_sprite;
+    Uint32 m_frame_display_time;
+    int m_current_frame;
+};
+
+SDL_Rect intersectRect(SDL_Rect* rect1, SDL_Rect* rect2);
+
+#endif // OBJECT_H

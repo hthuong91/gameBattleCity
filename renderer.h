fb81061e530b19f8513206f3a1c30f216c8a1a42
hthuong91 <24022741@vnu.edu.vn>
2025-04-25 00:41:34 +0700

    Create renderer.h

---
+++ renderer.h
@@ -0,0 +1,45 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <string>
+
+class Renderer
+{
+public:
+    Renderer();
+    ~Renderer();
+
+    void loadTexture(SDL_Window* window);
+
+    void loadFont();
+
+    void clear();
+
+    void flush();
+
+    void drawObject(const SDL_Rect *texture_src, const SDL_Rect *window_dest);
+
+    void setScale(float xs, float ys);
+
+    void drawText(const SDL_Point* start, std::string text, SDL_Color text_color, int font_size = 1);
+
+    void drawRect(const SDL_Rect* rect, SDL_Color rect_color, bool fill = false);
+
+private:
+
+    SDL_Renderer* m_renderer;
+
+    SDL_Texture* m_texture;
+
+    SDL_Texture* m_text_texture;
+
+    TTF_Font* m_font1;
+
+    TTF_Font* m_font2;
+
+    TTF_Font* m_font3;
+};
+
+#endif // RENDERER_H

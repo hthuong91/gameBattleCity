8b00f00607b1fed1bc6c7bdb155250cbf61f7d41
hthuong91 <24022741@vnu.edu.vn>
2025-04-25 00:40:58 +0700

    Update bullet.h

--- bullet.h
+++ bullet.h
@@ -1,19 +1,27 @@
 #ifndef BULLET_H
 #define BULLET_H
-#include<SDL.h>
-#include<vector>
-class playertank;
-class Game;
-class bullet
+
+#include "object.h"
+
+class Bullet : public Object
 {
-    public:
-        int x, y;
-        int dx, dy;
-        SDL_Rect rect;
-        bool active;
-        bullet(int startX, int startY, int dirX, int dirY);
-        void move();
-        void render(SDL_Renderer* renderer);
+public:
+
+    Bullet();
+
+    Bullet(double x, double y);
+
+    void update(Uint32 dt);
+
+    void destroy();
+
+    double speed;
+
+    bool collide;
+
+    bool increased_damage;
+
+    Direction direction;
 };
 
 #endif // BULLET_H

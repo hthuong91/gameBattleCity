2389bdd7189215be67bd26565e90cdea7f22c0a6
hthuong91 <24022741@vnu.edu.vn>
2025-04-25 03:28:07 +0700

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,38 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "appstate.h"
+#include "player.h"
+
+#include <vector>
+#include <string>
+
+class Menu : public AppState
+{
+public:
+    Menu();
+    ~Menu();
+
+    bool finished() const;
+
+    void draw();
+
+    void update(Uint32 dt);
+
+    void eventProcess(SDL_Event* ev);
+
+    AppState* nextState();
+
+private:
+
+    std::vector<std::string> m_menu_texts;
+
+    int m_menu_index;
+
+
+    Player* m_tank_pointer;
+
+    bool m_finished;
+};
+
+#endif // MENU_H

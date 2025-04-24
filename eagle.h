3f937a2177321902d8128d47290580ef1c40debe
hthuong91 <24022741@vnu.edu.vn>
2025-04-25 00:41:02 +0700

    Create eagle.h

---
+++ eagle.h
@@ -0,0 +1,18 @@
+#ifndef EAGLE_H
+#define EAGLE_H
+
+#include "object.h"
+
+class Eagle : public Object
+{
+public:
+    Eagle();
+
+    Eagle(double x, double y);
+
+    void update(Uint32 dt);
+
+    void destroy();
+};
+
+#endif // EAGLE_H

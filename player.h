62c2481411de439f7392dc8759a1aa9b65971d6c
hthuong91 <24022741@vnu.edu.vn>
2025-04-24 15:52:01 +0700

    Create player.h

---
+++ player.h
@@ -0,0 +1,34 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "tank.h"
+
+class Player : public Tank
+{
+public:
+    struct Playerkeys //Cấu trúc lưu trữ các phím điều khiển tương ứng để điều khiển xe tăng của người chơi.
+    {
+        PlayerKeys(): up(SDL_SCANCODE_UNKNOWN), down(SDL_SCANCODE_UNKNOWN), left(SDL_SCANCODE_UNKNOWN), right(SDL_SCANCODE_UNKNOWN), fire(SDL_SCANCODE_UNKNOWN){}
+        PlayerKeys(SDL_Scancode u, SDL_Scancode d, SDL_Scancode l, SDL_Scancode r, SDL_Scancode f): up(u), down(d), left(l), right(r), fire(f) {}
+        SDL_Scancode up; // đk lên trên
+        SDL_Scancode down;
+        SDL_Scancode left;
+        SDL_Scancode right;
+        SDL_Scancode fire; // bắn đạn
+    };
+
+    Player();
+    Player(double x , double y, SpriteType type);
+    void update(Uint32 dt);
+    void respawn;
+    void destroy;
+    Bullet* fire();
+    void changeStarCountBy(int c);
+    Playerkeys player_keys;
+    unsigned score;
+private:
+    int star_count;
+    Uint32 m_fire_time;
+};
+
+#endif // PLAYER_H

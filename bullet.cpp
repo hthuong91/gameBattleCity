5661cb0324b95cb5652f884642d367c4e95bd5af
hthuong91 <24022741@vnu.edu.vn>
2025-04-25 00:40:54 +0700

    Update bullet.cpp

--- bullet.cpp
+++ bullet.cpp
@@ -1,28 +1,103 @@
-
 #include "bullet.h"
-#include "game.h"
-bullet::bullet(int startX, int startY, int dirX, int dirY)
+#include "appconfig.h"
+
+Bullet::Bullet()
+    : Object(0, 0, ST_BULLET)
 {
-    x = startX;
-    y = startY;
-    dx = dirX;
-    dy = dirY;
-    active = true;
-    rect = {x, y, 10, 10} ;
+    speed = 0.0;
+    direction = D_UP;
+    increased_damage = false;
+    collide = false;
 }
-void bullet::move () {
-    x += dx;
-    y += dy;
-    rect.x = x;
-    rect.y = y;
-    if (x < TILE_SIZE || x > SCREEN_WIDTH - TILE_SIZE ||
-        y < TILE_SIZE || y > SCREEN_HEIGHT - TILE_SIZE) {
-            active = false;
+
+Bullet::Bullet(double x, double y)
+    : Object(x, y, ST_BULLET)
+{
+    speed = 0.0;
+    direction = D_UP;
+    increased_damage = false;
+    collide = false;
+}
+
+void Bullet::update(Uint32 dt)
+{
+    if(!collide)
+    {
+        switch (direction)
+        {
+        case D_UP:
+            pos_y -= speed * dt;
+            break;
+        case D_RIGHT:
+            pos_x += speed * dt;
+            break;
+        case D_DOWN:
+            pos_y += speed * dt;
+            break;
+        case D_LEFT:
+            pos_x -= speed * dt;
+            break;
         }
-}
-void bullet::render (SDL_Renderer* renderer) {
-    if (active) {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255) ;
-        SDL_RenderFillRect (renderer, &rect);
+
+        src_rect = moveRect(m_sprite->rect, direction, 0);
+        Object::update(dt);
     }
+    else
+    {
+        if(m_sprite->frames_count > 1)
+        {
+            m_frame_display_time += dt;
+            if(m_frame_display_time > m_sprite->frame_duration)
+            {
+                m_frame_display_time = 0;
+                m_current_frame++;
+                if(m_current_frame >= m_sprite->frames_count)
+                    to_erase = true;
+
+                src_rect = moveRect(m_sprite->rect, 0, m_current_frame);
+            }
+        }
+    }
+}
+
+void Bullet::destroy()
+{
+    if(collide) return; // ngăn việc gọi nhiều lần
+
+
+    collide = true;
+    speed = 0;
+    m_current_frame = 0;
+    m_frame_display_time = 0;
+    m_sprite = Engine::getEngine().getSpriteConfig()->getSpriteData(ST_DESTROY_BULLET);
+
+    switch(direction)
+    {
+    case D_UP:
+        dest_rect.x = pos_x + (dest_rect.w - m_sprite->rect.w) / 2; // dest_rect.w, dest_rect.h - kích thước cũ của viên đạn
+
+        dest_rect.y = pos_y - m_sprite->rect.h / 2;
+        break;
+    case D_RIGHT:
+        dest_rect.x = pos_x + dest_rect.w - m_sprite->rect.w / 2;
+        dest_rect.y = pos_y + (dest_rect.h - m_sprite->rect.h) / 2;
+        break;
+    case D_DOWN:
+        dest_rect.x = pos_x + (dest_rect.w - m_sprite->rect.w) / 2;
+        dest_rect.y = pos_y + dest_rect.h - m_sprite->rect.h / 2;
+        break;
+    case D_LEFT:
+        dest_rect.x = pos_x - m_sprite->rect.w / 2;
+        dest_rect.y = pos_y + (dest_rect.h - m_sprite->rect.h) / 2;
+        break;
+    }
+
+    dest_rect.h = m_sprite->rect.h;
+    dest_rect.w = m_sprite->rect.w;
+
+    src_rect.x = m_sprite->rect.x;
+    src_rect.y = m_sprite->rect.y;
+    src_rect.h = m_sprite->rect.h;
+    src_rect.w = m_sprite->rect.w;
+
 }

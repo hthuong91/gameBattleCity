a8c91a93e7145b393f4fdd57c2ac1331ce659b37
hthuong91 <24022741@vnu.edu.vn>
2025-04-27 02:11:53 +0700

    Update tank.h

--- tank.h
+++ tank.h
@@ -12,27 +12,28 @@ typedef unsigned TankStateFlags;
 class Tank : public Object // quản lý cơ chế cơ bản của xe tăng: di chuyển, bắn
 {
 public:
-    Tank(); //tạo xe tăng ở vị trí đầu tiên của kẻ địch
-    Tank(double x, double y, SpriteType type); // x vtri ban đầu ngang, y vtri ban dau doc, kieu xe tang
-    virtual ~Tank();
-    void draw();
-    void update(Uint32 dt);
-    virtual Bullet* fire();
-    SDL_Rect nextCollisionRect(Uint32 dt);
-    void setDirection(Direction d);
-    void collide(SDL_Rect &intersect_rect);
-    virtual void respawn();
-    virtual void destroy();
-    void setFlag(TankStateFlag flag);
-    void clearFlag(TankStateFlag flag);
-    bool testFlag(TankStateFlag flag);
+    Tank(); // Constructor mặc định
+    Tank(double x, double y, SpriteType type); // Constructor với vị trí và loại xe tăng
+    virtual ~Tank(); // Destructor
+    void draw(); // Vẽ xe tăng và các thành phần liên quan
+    void update(Uint32 dt); // Cập nhật trạng thái xe tăng theo thời gian
+    virtual Bullet* fire(); // Bắn đạn
+    SDL_Rect nextCollisionRect(Uint32 dt); // Tính toán hình chữ nhật va chạm tiếp theo
+    void setDirection(Direction d); // Đặt hướng di chuyển
+    void collide(SDL_Rect &intersect_rect); // Xử lý va chạm
+    virtual void respawn(); // Hồi sinh xe tăng
+    virtual void destroy(); // Hủy xe tăng
+    void setFlag(TankStateFlag flag); // Bật cờ trạng thái
+    void clearFlag(TankStateFlag flag); // Tắt cờ trạng thái
+    bool testFlag(TankStateFlag flag); // Kiểm tra cờ trạng thái
+
+    // Các biến thành viên
     double default_speed;
     double speed;
     bool stop;
     Direction direction;
     std::vector<Bullet*> bullets;
     int lives_count;
-
 protected:
     TankStateFlags m_flags;
     Sint32 m_slip_time;

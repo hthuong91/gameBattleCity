a924d324e4dd80a8f15c8eb3dd7984078d5e97f7
hthuong91 <24022741@vnu.edu.vn>
2025-05-01 02:35:20 +0700

    Update spriteconfig.cpp

--- spriteconfig.cpp
+++ spriteconfig.cpp
@@ -14,14 +14,11 @@ SpriteConfig::SpriteConfig()
     insert(ST_STONE_WALL, 928, 144, 16, 16, 1, 200, false);
     insert(ST_WATER, 928, 160, 16, 16, 2, 350, true);
     insert(ST_BUSH, 928, 192, 16, 16, 1, 200, false);
-    insert(ST_ICE, 928, 208, 16, 16, 1, 200, false);
 
     insert(ST_SHIELD, 976, 0, 32, 32, 2, 45, true);
     insert(ST_CREATE, 1008, 0, 32, 32, 10, 100, false);
     insert(ST_DESTROY_TANK, 1040, 0, 64, 64, 7, 70, false);
     insert(ST_DESTROY_BULLET, 1108, 0, 32, 32, 5, 40, false);
-    insert(ST_BOAT_P1, 944, 96, 32, 32, 1, 200, false);
-    insert(ST_BOAT_P2, 976, 96, 32, 32, 1, 200, false);
 
     insert(ST_EAGLE, 944, 0, 32, 32, 1, 200, false);
     insert(ST_DESTROY_EAGLE, 1040, 0, 64, 64, 7, 100, false);

ee1cfb5cd3514e872793dafedf65a19e6fcb8138
hthuong91 <24022741@vnu.edu.vn>
2025-05-03 22:34:58 +0700

    Update app.h

--- app.h
+++ app.h
@@ -1,7 +1,7 @@
 #ifndef APP_H
 #define APP_H
 #include "appstate.h"
-
+#include<SDL_mixer.h>
 class App
 {
     public:
@@ -9,6 +9,8 @@ class App
         ~App(); // destrctor, hủy đối tượng
         void run(); // hàm chạy app
         void eventProces(); // hàm xử lí sự kiện
+        Mix_Music* bgm = nullptr;
+
     private:
         bool is_running; // biến kiểm tra ứng dụng có đang chạy hay không
         AppState* m_app_state; // trạng thái hiện tại của ứng dụng (vd menu, game,...)

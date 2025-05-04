eba94182fd0904fadc2230b90c62a5b0d326a331
hthuong91 <24022741@vnu.edu.vn>
2025-05-04 11:16:13 +0700

    Update bullet.h

--- bullet.h
+++ bullet.h
@@ -2,13 +2,14 @@
 #define BULLET_H
 #include<SDL.h>
 #include"object.h"
+#include<SDL_mixer.h>
 class Bullet : public Object
 {
 public:
 
     Bullet();
 
-    Bullet(double x, double y);
+    Bullet(double x, double y, bool from_player = false);
 
     void update(Uint32 dt);
 
@@ -21,6 +22,13 @@ public:
     bool increased_damage;
 
     Direction direction;
+
+    static bool loadAssets();
+    static void cleanupAssets();
+
+private:
+    static Mix_Chunk* sfx_shoot;
+    bool is_player_bullet;
 };
 
 #endif // BULLET_H

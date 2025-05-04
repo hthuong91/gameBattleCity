16f0cb96fb2e2dda2c7e651c2dc44d70a95b4e3a
hthuong91 <24022741@vnu.edu.vn>
2025-05-04 11:16:01 +0700

    Update app.h

--- app.h
+++ app.h
@@ -11,6 +11,7 @@ class App
         void eventProces(); // hàm xử lí sự kiện
         Mix_Music* bgm = nullptr;
 
+
     private:
         bool is_running; // biến kiểm tra ứng dụng có đang chạy hay không
         AppState* m_app_state; // trạng thái hiện tại của ứng dụng (vd menu, game,...)
